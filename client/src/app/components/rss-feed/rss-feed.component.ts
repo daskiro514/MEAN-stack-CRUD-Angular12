1b597b17a6cb857cf5376e0d34609afb92ce8d76
Ye_Altman <ye.altman@gmail.com>
2021-11-18 09:18:27 +0200

    improve rss feed

--- client/src/app/components/rss-feed/rss-feed.component.ts
+++ client/src/app/components/rss-feed/rss-feed.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { interval, of } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
-import { Rss } from 'src/app/models/rss.model';
+import { interval, Observable, timer } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { RssViewModel } from 'src/app/models/rss.model';
 import { RssConfiguration } from 'src/app/models/rssconfiguration.model';
 import { RssService } from 'src/app/services/rss.service';
 
@@ -12,40 +12,17 @@ import { RssService } from 'src/app/services/rss.service';
 })
 
 export class RssFeedComponent implements OnInit {
-  configuration: RssConfiguration = {
-    source: '',
-    refreshTime: 1,
-    bgColor: '',
-    color: ''
-  };
+  viewModel$?: Observable<RssViewModel>;
 
-  rsses?: Rss[];
+  constructor(private readonly rssService: RssService) { }
 
-  baseUrl = 'https://test.urban-digital.co.il:7012/ExternalData/108'
-
-  constructor(private rssService: RssService) { }
-
-  ngOnInit(): void {
-    this.retrieveRsses();
-  }
-
-  retrieveRsses(): void {
-    this.rssService.getConfiguration().subscribe(
-      config => {
-        if (config) {
-          this.configuration = config
-          interval(config.refreshTime * 60 * 1000).pipe(
-            switchMap(() => this.rssService.getRssFeed(`${this.baseUrl}?source=${config.source}&bgColor=${config.bgColor}&color=${config.color}`))
-          ).subscribe(
-            data => {
-              this.rsses = data;
-            },
-            error => {
-              console.log(error);
-            }
-          )
-        }
-      }
-    )
+  ngOnInit() {
+    this.viewModel$ = this.rssService.getConfiguration()
+      .pipe(map<RssConfiguration, RssViewModel>(config => ({
+        bgColor: config.bgColor,
+        color: config.color,
+        feed: timer(0, (config.refreshTime || 10) * 60 * 1000)
+          .pipe(switchMap(() => this.rssService.getRssFeed(config.source)))
+      })));
   }
 }
